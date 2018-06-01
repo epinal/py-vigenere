fd7e5c89dcab6551620c7f1358012b77f0f323ac
Ernesto <21268032+epinal@users.noreply.github.com>
2018-05-31 23:20:02 -0400

    First commit

---
+++ vigenere_cypher.py
@@ -0,0 +1,38 @@
+from itertools import cycle
+import string
+
+EMPTY_STR = ''
+SPACE = ' '
+
+
+class VigenereCypher:
+    def __init__(self, alphabet: str = string.ascii_uppercase):
+        self._alphabet = alphabet
+        self._alphabet_len = len(alphabet)
+
+    def encrypt(self, key_word: str, text: str) -> str:
+        key_iter = cycle(key_word)
+        cipher = EMPTY_STR
+        for letter in text:
+            letter_pos = self._alphabet.find(letter.upper())
+            key_letter_pos = self._alphabet.find(next(key_iter).upper())
+            cipher += self._alphabet[(letter_pos + key_letter_pos) % self._alphabet_len] if letter != SPACE else letter
+
+        return cipher
+
+    def decrypt(self, key_word: str, cypher_text: str) -> str:
+        key_iter = cycle(key_word)
+        plain_text = EMPTY_STR
+        for letter in cypher_text:
+            letter_pos = self._alphabet.find(letter.upper())
+            key_letter_pos = self._alphabet.find(next(key_iter).upper())
+            plain_text += self._alphabet[
+                (letter_pos - key_letter_pos) % self._alphabet_len] if letter != SPACE else letter
+
+        return plain_text
+
+
+if __name__ == '__main__':
+    vigenere = VigenereCypher()
+    print(vigenere.encrypt('secret', 'ernesto luis'))
+    print(vigenere.decrypt('secret', 'wvpvwmg nlml'))
